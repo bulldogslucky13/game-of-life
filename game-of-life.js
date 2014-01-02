bf5f44c5771a29111e1644de75e170a48b6dd44e
bulldogslucky13 <cameronbristol@Camerons-MacBook-Pro.local>
2014-01-01 18:11:58 -0600

    Minor tweeks on the prompt

---
+++ game-of-life.js
@@ -0,0 +1,24 @@
+
+
+
+
+var user = prompt("What is your name?");
+
+var output_div = document.getElementById('output');
+output_div.innerHTML = "Welcome Master " + user + "! I am so glad you are here!";
+
+console.log('Get used to opening the developer tools!');
+
+var grid = document.getElementById('grid');
+
+var number_of_columns = 10;
+var number_of_rows = 10;
+var current_column;
+var current_row;
+var current_cell;
+
+for ( current_row = 0; current_row < number_of_rows; current_row++ ) {
+    grid.innerHTML = grid.innerHTML + '<div class="row">ROW ' + current_row + '</div>';
+
+
+}
